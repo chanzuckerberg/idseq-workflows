676fec90ee81e7b926c4b88c817423e80852514f
morsecodist <todd@morsecodist.io>
2021-01-22 13:47:34 -0500

    exp

--- tests/short-read-mngs/non_host_alignment/test_RunAlignment.py
+++ tests/short-read-mngs/non_host_alignment/test_RunAlignment.py
@@ -5,7 +5,7 @@ import json
 def test_RunValidateInput_invalid(
     repo_dir, short_read_mngs_bench3_viral_outputs, miniwdl_inputs_outputs, miniwdl_run, RunFailed_stderr_msg
 ):
-    foo = os.listdir(os.path.join(short_read_mngs_bench3_viral_outputs["dir"], "call-non_host_alignment")
+    foo = os.listdir(os.path.join(short_read_mngs_bench3_viral_outputs["dir"], "call-non_host_alignment"))
     assert False, foo
     # load the task's inputs from the end-to-end workflow test
     inputs, _ = miniwdl_inputs_outputs(

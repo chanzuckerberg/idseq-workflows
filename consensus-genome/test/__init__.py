62d3f720d4872fbda316ad30e6b05f133c4746d6
Andrey Kislyuk <kislyuk@gmail.com>
2021-03-07 00:01:00 +0000

    Add output assertions

--- consensus-genome/test/__init__.py
+++ consensus-genome/test/__init__.py
@@ -30,7 +30,23 @@ class TestConsensusGenomes(TestCase):
         fastqs_1 = os.path.join(os.path.dirname(__file__), "sample_sars-cov-2_paired_r2.fastq.gz")
         args = ["sample=test_sample", f"fastqs_0={fastqs_0}", f"fastqs_1={fastqs_1}", "technology=Illumina"]
         res = self.run_miniwdl(args)
-        print(res)
+        outputs = res["outputs"]
+        with open(outputs["consensus_genome.compute_stats_out_output_stats"]) as fh:
+            output_stats = json.load(fh)
+        self.assertEqual(output_stats["sample_name"], "test_sample")
+        self.assertGreater(output_stats["depth_avg"], 889)
+        self.assertEqual(output_stats["total_reads"], 187444)
+        self.assertEqual(output_stats["mapped_reads"], 187212)
+        self.assertEqual(output_stats["mapped_paired"], 187151)
+        self.assertEqual(output_stats["ercc_mapped_reads"], 0)
+        self.assertEqual(output_stats["ref_snps"], 7)
+        self.assertEqual(output_stats["ref_mnps"], 0)
+        for output_name, output in outputs.items():
+            if not isinstance(output, list):
+                output = [output]
+            for filename in output:
+                with open(filename, "rb") as fh:
+                    self.assertGreater(os.path.getsize(filename), 0)
 
     def test_zip_outputs(self):
         res = self.run_miniwdl(task="ZipOutputs", args=["prefix=test", "outputFiles=run.wdl"])

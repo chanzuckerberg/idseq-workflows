25f4ced677c35803d81559e1fd24dbf0250f78a4
Mike Lin <dna@mlin.net>
2021-03-17 17:22:05 -1000

    wip

--- tests/short-read-mngs/host_filter/test_RunValidateInput.py
+++ tests/short-read-mngs/host_filter/test_RunValidateInput.py
@@ -1,20 +1,23 @@
+import sys
 import os
 import json
 
 
-def test_RunValidateInput_invalid(
-    repo_dir, short_read_mngs_bench3_viral_outputs, miniwdl_inputs_outputs, miniwdl_run, RunFailed_stderr_msg
-):
+def test_RunValidateInput_invalid(util, short_read_mngs_bench3_viral_outputs):
     # load the task's inputs from the end-to-end workflow test
-    inputs, _ = miniwdl_inputs_outputs(
-        os.path.join(short_read_mngs_bench3_viral_outputs["dir"], "call-host_filter/call-RunValidateInput")
+    inputs, _ = util.miniwdl_inputs_outputs(
+        os.path.join(
+            short_read_mngs_bench3_viral_outputs["dir"], "call-host_filter/call-RunValidateInput"
+        )
     )
     # override fastqs to invalid test article
-    inputs["fastqs"] = [os.path.join(os.path.dirname(__file__), "test_RunValidateInput_invalid.fastq")]
+    inputs["fastqs"] = [
+        os.path.join(os.path.dirname(__file__), "test_RunValidateInput_invalid.fastq")
+    ]
 
     # run the task with the manipulated inputs, expecting an error exit status
-    outp = miniwdl_run(
-        os.path.join(repo_dir, "short-read-mngs/host_filter.wdl"),
+    outp = util.miniwdl_run(
+        util.repo_dir() / "short-read-mngs/host_filter.wdl",
         "--task",
         "RunValidateInput",
         "-i",
@@ -23,6 +26,6 @@ def test_RunValidateInput_invalid(
     )
 
     # verify the error is as expected
-    err = RunFailed_stderr_msg(outp)
+    err = util.wdl_error_message(outp)
     assert err["wdl_error_message"]
     assert err["error"] == "InvalidFileFormatError"

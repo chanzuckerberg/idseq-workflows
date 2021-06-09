486a773cd8b157998cbae280e4df1cfac6db52cc
morsecodist <todd@morsecodist.io>
2021-06-09 18:44:14 -0400

    address non-determinism

--- consensus-genome/test/test_wdl.py
+++ consensus-genome/test/test_wdl.py
@@ -212,7 +212,7 @@ class TestConsensusGenomes(WDLTestCase):
         self.assertEqual(output_stats["ercc_mapped_reads"], 0)
         self.assertEqual(output_stats["ref_snps"], 0)
         self.assertEqual(output_stats["ref_mnps"], 0)
-        self.assertEqual(output_stats["n_actg"], 15313)
+        self.assertIn(output_stats["n_actg"], [15313, 15314])
         self.assertEqual(output_stats["n_missing"], 0)
         self.assertEqual(output_stats["n_gap"], 0)
         self.assertEqual(output_stats["n_ambiguous"], 4)

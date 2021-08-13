5ea8cdf6ca02097b33e6c84887119d320f41de0f
Todd Morse <todd@morsecodist.io>
2021-08-13 14:52:38 -0400

    Update test_RunIDSeqDedup.py

--- short-read-mngs/test/host_filter/test_RunIDSeqDedup.py
+++ short-read-mngs/test/host_filter/test_RunIDSeqDedup.py
@@ -46,6 +46,7 @@ def test_RunIDSeqDedup_safe_csv(util, short_read_mngs_bench3_viral_outputs):
                         found_quotes += 1
                         continue
                     assert elem[0].isalnum(), f"cell starts with a special character '{elem}'"
+        assert found_quotes == 10
     finally:
         for f in input_files:
             f.close()

bbd64e285b16e7a284fdb27d6835896cc7db1320
morsecodist <todd@morsecodist.io>
2021-08-16 10:50:57 -0400

    cleanup

--- short-read-mngs/test/host_filter/test_RunIDSeqDedup.py
+++ short-read-mngs/test/host_filter/test_RunIDSeqDedup.py
@@ -13,10 +13,11 @@ def test_RunIDSeqDedup_safe_csv(util, short_read_mngs_bench3_viral_outputs):
     )
 
     with NamedTemporaryFile(prefix=os.path.dirname(__file__), mode="w") as input_file:
+        quote_count = 10
         special_char_rows = 0
         for line in open(inputs["priceseq_fa"][0]):
             if line[0] == ">" or line[0] == "@":
-                if special_char_rows < 10:
+                if special_char_rows < quote_count:
                     input_file.write(f"{line[0]}={line[1:]}")
                     special_char_rows += 1
                 else:
@@ -25,7 +26,7 @@ def test_RunIDSeqDedup_safe_csv(util, short_read_mngs_bench3_viral_outputs):
                 input_file.write(line)
 
         input_file.seek(0)
-        assert special_char_rows == 10
+        assert special_char_rows == quote_count
 
         inputs["priceseq_fa"] = [input_file.name]
 
@@ -40,15 +41,13 @@ def test_RunIDSeqDedup_safe_csv(util, short_read_mngs_bench3_viral_outputs):
         dups = outp["outputs"]["RunIDSeqDedup.duplicate_clusters_csv"]
 
         found_quotes = 0
-        rows = 0
         # check we have an quotes before special characters space to prevent CSV injection
         with open(dups) as f:
             for row in csv.reader(f):
-                rows += 1
                 for i, elem in enumerate(row):
                     if elem[0] == "'":
                         if i == 1:
                             found_quotes += 1
                         continue
                     assert elem[0].isalnum(), f"cell starts with a special character '{elem}'"
-        assert found_quotes == 10
+        assert found_quotes == quote_count
